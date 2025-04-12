e402fdbfa338aede84e162a6760020920296254d
hjk1996 <dunhill741@naver.com>
2025-04-12 16:12:48 +0900

    page 이름 변경

--- application/src/housing_alert/pages/notification.py
+++ application/src/housing_alert/pages/notification.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from housing_alert.services import db, storage, ai
 
+params = st.query_params
+
+user = params.get("user", None)
+
+
 st.title("공고 알람")
-st.markdown(
-    "공고 알람"
-)
+st.markdown("공고 알람")
+
+
+
